c64749b0716b9d0382ebab354720634810865436
Leo mini <a230711@yahoo.com.tw>
2022-12-03 00:48:48 +0800

    useContext

--- src/App.js
+++ src/App.js
@@ -1,20 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
-import Test from './Test/Parent';
-import Component from './Context/Component';
-import { useState } from 'react';
-import { createContext } from 'react';
-export const ThemeContext = createContext();
+// import React, { Component } from "react";
+// // import Button, {Nav} from "./Test/Button";
+// import Thisuse from './Thisuse';
 
-function App() {
-  const [dark, setDark] = useState(true);
-  console.log(dark);
+// // class Nav extends Component{
+// //   constructor(){
+// //     super()
+// //   }
+// //   render(){
+// //     return(
+// //       <div style={{backgroundColor:"black", width:"100%", color:"white"}}>Nav</div>
+// //     )
+// //   }
+// // }
+// // function Button(){
+// //   return(
+// //     <button type="button">click</button>
+// //   )
+// // }
+
+// class App extends Component{
+//   constructor(){
+//     super()
+//     this.state = {
+//       like : true
+//     }
+//   }
+//   hander(){
+//     this.setState({
+//       like: !this.state.like
+//     })
+//   }
+//   render(){
+//     return(
+//       <Thisuse />
+//       // <div>
+//       //   <Nav />
+//       //   <Button/>
+//       //   <Button/>
+//       //   <Button/>
+//       //   <Button/>
+//       //   <button type="button"
+//       //     onClick={()=>{
+//       //       this.hander()
+//       //     }}
+//       //   >
+//       //   {this.state.like?"like":"Thank"}
+//       //   </button>
+//       // </div>
+      
+//     )
+//   }
+// }
+
+import React, {useState} from "react";
+// import FunctionContextComponent from './FunctionContextComponent';
+import ClassContextComponent from './ClassContextComponent';
+
+export const ThemeContext = React.createContext()
+
+export default function App(){
+  const [darkTheme, setDarkTheme] = useState(true)
+
+  function toggleTheme(){
+    setDarkTheme(prevDarkTheme => !prevDarkTheme)
+  }
   return (
-    // <Test />
-    <ThemeContext.Provider value={dark}>
-      <Component />
-    </ThemeContext.Provider>
-  );
+    <>
+      <ThemeContext.Provider value={darkTheme}>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+        {/* <FunctionContextComponent /> */}
+        <ClassContextComponent />
+      </ThemeContext.Provider>
+    </>
+  )
 }
 
-export default App;
+// import React from "react";
+// import FunctionContextComponent from './FunctionContextComponent';
+// import ClassContextComponent from './ClassContextComponent';
+// import { ThemeProvider } from "./ThemeContext";
+
+// export default function App(){
+//   return (
+//     <>
+//       <ThemeProvider>
+//         <FunctionContextComponent />
+//       </ThemeProvider>
+//     </>
+//   )
+// }
+
