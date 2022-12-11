61bec1f3f39366de5dfed7dc369ce6179c11462c
Leo mini <a230711@yahoo.com.tw>
2022-12-12 00:04:36 +0800

    new life use have promblem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import Btn from './Btn';
+// import Btn from './Btn';
+import Demo from './Demo';
+import './App.scss'
 
 class App extends Component{
   constructor(porps){
@@ -9,20 +11,21 @@ class App extends Component{
     }
   }
 
-  componentWillMount(){
-    console.log('組件將要安裝compontWillMount')
-  }
-  componentDidMount(){
-    console.log('組件安裝完成compontDidMount')
-  }
+  
+  // componentWillMount(){
+  //   console.log('組件將要安裝compontWillMount')
+  // }
+  // componentDidMount(){
+  //   console.log('組件安裝完成compontDidMount')
+  // }
 
   render(){
     console.log('render')
     return(
-      <div>
-        {this.state.isIf ? <Btn /> : "不宣染"}
+      <div className="app">
+        {this.state.isIf ? <Demo /> : "不宣染"}
         <p>App</p>
-        <button type="button" onClick={()=>this.setState({})}>App</button>
+        <button type="button" className="btn" onClick={()=>this.setState({})}>App</button>
         <button type="button" onClick={()=>this.setState({isIf: !this.state.isIf})}>Alick</button>
       </div>
     )
