e7d1e10ac5b50c884eb3b5e0e5c8dc4d26c4d0a3
Leo mini <a230711@yahoo.com.tw>
2022-12-21 20:39:14 +0800

    new context

--- src/App.js
+++ src/App.js
@@ -1,55 +1,133 @@
 import React, { Component } from "react";
 // import Btn from './Btn';
-import Demo from './Demo';
-import './App.scss'
+// import Demo from './Demo';
+// import './App.scss'
+import PropTypes from "prop-types";
 
-class Title extends Component{
-  constructor(){
-    super()
-  }
+// class Title extends Component{
+//   // static contextTypes = {
+//   //   themeColor : PropTypes.string,
+//   //   handleChangeThemeColor : PropTypes.func
+//   // }
+//   render(){
+//     console.log(this.props);
+//     console.log(this.context);
+//     const {themeColor, handleChangeThemeColor} = this.context
+//     return(
+//       <h1 style={{color : themeColor}}>大標題</h1>
+//     )
+//   }
+// }
+// // Title.contextTypes = {
+// //   themeColor : PropTypes.string,
+// //   handleChangeThemeColor : PropTypes.func
+// // }
 
-  shouldComponentUpdate(nextProps){
-    console.log(`nextprops:${nextProps.title}`);
-    console.log(`props:${this.props.title}`);
-    if(nextProps.title !== this.props.title){
-      return true
-    }
-    return false
-  }
+// class Button extends Component{
+//   // static contextTypes = {
+//   //   themeColor : PropTypes.string,
+//   //   handleChangeThemeColor : PropTypes.func
+//   // }
+//   render(){
+//     const {themeColor, handleChangeThemeColor} = this.context
+//     return(
+//       <div>
+//         <button type="button" style={{color : themeColor}}
+//         onClick={()=>{handleChangeThemeColor('red')}}
+//         >Red</button>
+//         <button type="button" style={{color : themeColor}}
+//         onClick={()=>{handleChangeThemeColor('green')}}
+//         >Green</button>
+//       </div>
+//     )
+//   }
+// }
+// // Button.contextTypes = {
+// //   themeColor : PropTypes.string,
+// //   handleChangeThemeColor : PropTypes.func
+// // }
 
-  render(){
-    return(
-      <h1>{this.props.title}</h1>
-    )
+// class App extends Component{
+//   // static childContextTypes = {
+//   //   themeColor : PropTypes.string,
+//   //   handleChangeThemeColor : PropTypes.func
+//   // }
+//   constructor(){
+//     super()
+//     this.state = {
+//       themeColor : 'red'
+//     }
+//   }
+//   handleChangeThemeColor(color){
+//     this.setState({
+//       themeColor : color
+//     })
+//   }
+//   // getChildContext(){
+//   //   return {
+//   //     themeColor : this.state.themeColor,
+//   //     handleChangeThemeColor : (color)=>this.handleChangeThemeColor(color)
+//   //   }
+//   // }
+//   render(){
+//     return(
+//       <div>
+//         <Title></Title>
+//         <Button></Button>
+//       </div>
+//     )
+//   }
+// }
+// // App.childContextTypes = {
+// //   themeColor : PropTypes.string,
+// //   handleChangeThemeColor : PropTypes.func
+// // }
+
+// Context lets us pass a value deep into the component tree
+// without explicitly threading it through every component.
+// Create a context for the current theme (with "light" as the default).
+const ThemeContext = React.createContext('light');
+
+class App extends React.Component {
+  render() {
+    // Use a Provider to pass the current theme to the tree below.
+    // Any component can read it, no matter how deep it is.
+    // In this example, we're passing "dark" as the current value.
+    return (
+      <ThemeContext.Provider value="dark">
+        <Toolbar />
+      </ThemeContext.Provider>
+    );
   }
 }
 
-class App extends Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      title: 'hello world1',
-      num : 0
-    }
-  }
+// A component in the middle doesn't have to
+// pass the theme down explicitly anymore.
+function Toolbar() {
+  return (
+    <div>
+      <ThemedButton />
+      <ThemedAutton />
+    </div>
+  );
+}
 
+class ThemedButton extends React.Component {
+  // Assign a contextType to read the current theme context.
+  // React will find the closest theme Provider above and use its value.
+  // In this example, the current theme is "dark".
+  // static contextType = ThemeContext;
+  render() {
+    // return <Button theme={this.context} />;
+    return <p>{this.context}</p>
+  }
+}
+ThemedButton.contextType = ThemeContext;
+
+class ThemedAutton extends React.Component {
+  static contextType = ThemeContext;
   render(){
-    const {title, nun} = this.state;
-    return(
-      <div>
-        <h1>{this.state.num}</h1>
-        <Title title = {title}></Title>
-        <button type="button"
-        onClick={()=>{this.setState({num : this.state.num + 1})}}>
-          Click
-        </button>
-        <button onClick={() => {
-            this.setState({
-                title: Math.random()
-            })
-        }}>change title</button>
-      </div>
-    )
+    return <p>{this.context}</p>
   }
 }
 
